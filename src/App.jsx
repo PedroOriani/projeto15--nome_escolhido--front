cd8156c5c483d7250b3056105f44e2bf1d9be524
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-07-12 10:07:55 -0300

    Arquiteura do Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SignIn from './pages/SignIn/SignIn'
+import SignUp from './pages/SignUp/SignUp'
+import HomePage from './pages/HomePage/HomePage'
+import ShoppingCart from './pages/ShoppingCart/ShoppingCart'
+import Checkout from './pages/Checkout/Checkout'
+
 function App() {
-  
 
   return (
-    <>
-      <p>Driven Tech</p>
-    </>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={ <SignIn /> }/>
+        <Route path='/register' element={ <SignUp /> }/>
+        <Route path='home' element={ <HomePage /> }/>
+        <Route path='cart' element={ <ShoppingCart /> }/>
+        <Route path='checkout' element={ <Checkout /> }/>
+
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
 
