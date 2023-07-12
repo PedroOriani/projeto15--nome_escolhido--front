14965e5711c8957d9f3c7fe3aa9cc5bc0db02fb4
nilteras <niltonrnascimento@hotmail.com>
2023-07-12 15:53:09 -0300

    feat: Layout initial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,74 @@
-export default function Header(){
-    return(
-        <p>Header</p>
+import styled from 'styled-components'
+
+export default function Header() {
+
+    return (
+        <>
+            <HeaderContainer>
+                <MenuContainer>
+                    <Acess>
+                        <p>Login</p>
+                        <p>Cadastro</p>
+                        <p>Home</p>
+                    </Acess>
+
+                    <Cart>
+                        <p>icone</p>
+                        <p>Carrinho</p>
+                    </Cart>
+                </MenuContainer>
+                <LogoContainer>
+                    <h1>DrivenTech</h1>
+                </LogoContainer>
+            </HeaderContainer>
+
+
+        </>
     )
 }
+
+const HeaderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const MenuContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 55px;
+    padding: 15px;
+    
+`
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 120px;
+    background-color: #255566;
+    h1 {
+        font-family: 'Montserrat';
+        font-size: 30px;
+        font-weight: 600;
+        color: white
+    }
+    
+   
+`
+const Acess = styled.div`
+    display: flex;
+    p {
+        font-family: 'Roboto';
+        font-weight: 400;
+        margin-right: 10px;
+    }
+`
+const Cart = styled.div`
+    display: flex;
+    
+    p {
+        font-family: 'Roboto';
+        font-weight: 400;
+        margin-left: 10px;
+    }
+`
