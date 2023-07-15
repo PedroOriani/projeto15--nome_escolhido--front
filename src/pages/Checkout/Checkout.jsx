9ad8336238d3a706475adff99c1e8203a17416c4
nilteras <niltonrnascimento@hotmail.com>
2023-07-15 11:10:30 -0300

    refactor layout Checkout Page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,112 +1,61 @@
 import { useContext } from "react"
 import PathContext from "../../context/PathContext"
 import Header from "../../components/Header"
-import styled from 'styled-components'
 import notebook from './../../../assets/notebook.png'
+import { PageContainer, InfoContainer, Info, UserInfo, AddAdress, AddPay, Subtotal, SubtotalInformation } from "./styleCheckout"
 
 export default function Checkout() {
 
-    const { setPath } = useContext(PathContext)
+  const { setPath } = useContext(PathContext)
 
-    setPath('checkout')
+  setPath('checkout')
 
-    return (
-        <>
-            <Header />
-            <PageContainer>
-            <ShoppingCartContainer>
-            <h1>Informações</h1>
-            <hr />
-            <Product>
-              <ProductInfo>
-                <p>NOME DO PESSOA</p>
-                <p>email@email.com</p>
-                <p>ENTREGA</p>
-                <p>Adicionar novo endereço</p>
-                <p>Volte as compras</p>
-              </ProductInfo>
-            </Product>
-          </ShoppingCartContainer>
-                <Subtotal>
-                    <h1>Resumo da Compra</h1> <p>1 Item</p>
-                    <img src={notebook} alt="" />
-                    <p>Notebook dell 8GB 1TB</p>
-                    <p>R$ 2698,00</p><br />
-                    <p>Subtotal: R$ 2698,00</p>
-                    <p>Frete: A calcular</p>
-                    <p>Total: R$ 2698,00</p>
-                </Subtotal>
-            </PageContainer>
 
-        </>
-    )
+
+  return (
+    <>
+      <Header />
+      <PageContainer>
+        <InfoContainer>
+          <h1>Informações</h1>
+          <hr />
+          <Info>
+            <UserInfo>
+              <p>Fulando da Silva</p>
+              <p>email@email.com</p>
+              <h3>ENTREGA</h3>
+              <p>Endereço: </p>
+              <div>
+                <AddAdress><h2>Adicionar novo endereço</h2></AddAdress>
+                <AddPay><h2>Salvar e escolher forma de pagamento</h2></AddPay>
+              </div>
+
+              <p>Adicionar mais itens</p>
+            </UserInfo>
+          </Info>
+        </InfoContainer>
+        <Subtotal>
+          <div>
+            <h1>Resumo da Compra</h1> <p>1 Item</p>
+          </div>
+          <div>
+            <img src={notebook} alt="" />
+            <p>Notebook dell 8GB 1TB</p>
+            <p>R$ 2698,00</p>
+          </div>
+          <hr />
+          <SubtotalInformation>
+            <p>Subtotal:</p> <p>R$ 2698,00</p>
+          </SubtotalInformation>
+          <SubtotalInformation>
+            <p>Frete:</p> <p>A calcular</p>
+          </SubtotalInformation>
+          <SubtotalInformation>
+            <p>Total:</p> <p>R$ 2698,00</p>
+          </SubtotalInformation>
+        </Subtotal>
+      </PageContainer>
+
+    </>
+  )
 }
-
-const Subtotal = styled.div`
-  width: 20%;
-  height: 100%;
-  margin-top: 20px;
-  background: #F5F5F5;
-  display: flex;
-  flex-direction: column;
-  row-gap: 10px;
-  padding: 20px;
-  h1 {
-    font-size: 20px;
-  }
-  strong {
-    font-weight: 600;
-  }
-  button {
-    font-size: 16px;
-    border: none;
-    background-color: #255566;
-    color: white;
-    border-radius: 10px;
-    height: 30px;
-  }
-`
-const PageContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  font-family: 'Roboto';
-`
-
-const ShoppingCartContainer = styled.div`
-  width: 75%;
-  background: #F5F5F5;
-  padding: 20px;
-  margin-top: 20px;
-  h1 {
-    font-size: 24px;
-  }
-  hr {
-    color: #F8F8F8;
-  }
-`
-
-const Product = styled.div`
-  display: flex;
-  gap: 20px;
-  margin-top: 20px;
-  img {
-    width: 300px;
-  }
-`
-
-const ProductInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 10px;
-  p:nth-child(1) {
-    font-size: 20px;
-  }
-  p:nth-child(2) {
-    font-size: 20px;
-    font-weight: 600;
-  }
-  p:nth-child(3) {
-    font-size: 15px;
-    color: #282828;
-  }
-`
---
+++ src/pages/Checkout/styleCheckout.js
@@ -0,0 +1,136 @@
+import styled from 'styled-components'
+
+
+export const Subtotal = styled.div`
+  width: 35%;
+  height: 100%;
+  margin-top: 20px;
+  background: #F5F5F5;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  padding: 20px;
+  h1 {
+    font-size: 20px;
+    font-weight: 600;
+    margin-bottom: 8px;
+  }
+  strong {
+    font-weight: 600;
+  }
+  button {
+    font-size: 16px;
+    border: none;
+    background-color: #255566;
+    color: white;
+    border-radius: 10px;
+    height: 30px;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  img {
+    width: 120px;
+  }
+`
+export const SubtotalInformation = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+`
+export const PageContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-family: 'Roboto';
+`
+
+export const InfoContainer = styled.div`
+  width: 45%;
+  background: #F5F5F5;
+  padding: 20px;
+  margin-top: 20px;
+  h1 {
+    font-size: 24px;
+  }
+  hr {
+    color: #F8F8F8;
+  }
+`
+
+export const Info = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-top: 20px;
+
+`
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  width: 100%;
+  p:nth-child(1) {
+    font-size: 20px;
+  }
+  p:nth-child(2) {
+    font-size: 20px;
+    font-weight: 600;
+  }
+  p:nth-child(3) {
+    font-size: 15px;
+    color: #282828;
+  }
+  h3 {
+    margin-top: 15px;
+    font-style: italic;
+  }
+  p:nth-child(6) {
+    font-size: 18px;
+    font-weight: 700;
+    color: red;
+  }
+  div {
+    display: flex;
+    justify-content: space-around;
+  }
+`
+export const AddAdress = styled.button`
+    width: 40%;
+    height: 40px;
+    background-color:white;
+    
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    /* border-radius: 20px; */
+
+    h2 {
+        font-family: 'Roboto';
+        font-weight: 400;
+        font-size: 15px;
+        color: black;
+    }
+`
+export const AddPay = styled.button`
+    width: 40%;
+    height: 40px;
+    background-color: green;
+    
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    /* border-radius: 20px; */
+
+    h2 {
+        font-family: 'Roboto';
+        font-weight: 400;
+        font-size: 15px;
+        color: white;
+    }
+`
